8ef11e7d3e965048f9bb53310ca7247a7ef9a12d
Daniel Molloy <danielmolloy_6@icloud.com>
2023-07-12 16:49:22 +0100

    staticfiles

--- myjournal/settings.py
+++ myjournal/settings.py
@@ -21,8 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
-#'django-insecure-b%=p1pg@s%lghe2i$grc_g5q^i%h4gtdr7*c)3k*m2n_5&bpo4'
+#SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -44,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -118,14 +118,21 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'
-""" 
+STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+) 
+"""STATICFILES_DIRS = [
+    BASE_DIR / 'journal/static',
+]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 ) """
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- myjournal/urls.py
+++ myjournal/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 
 
 urlpatterns = [
-    path("journal/", include("journal.urls")),
+    path("", include("journal.urls")),
     path('admin/', admin.site.urls),
 ]
 
