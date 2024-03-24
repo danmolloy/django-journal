71c7a6470b6d7c08c6b1488810ab14141fcf95b2
Daniel Molloy <danielmolloy_6@icloud.com>
2024-03-24 13:28:27 +0000

    bug fix

--- myjournal/settings.py
+++ myjournal/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-CSRF_TRUSTED_ORIGINS = ['https://*.railwayapp.com']
-ALLOWED_HOSTS = ["*", "127.0.0.1", "https://*.railwayapp.com"]
+CSRF_TRUSTED_ORIGINS = ['https://django-journal-production.up.railway.app/']
+ALLOWED_HOSTS = ["*", "127.0.0.1", "*.railway.app"]
 
 
 # Application definition
