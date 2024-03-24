0fa89dbf1f14d0934ef8b0aebbd742ce7d0b532b
Daniel Molloy <danielmolloy_6@icloud.com>
2024-03-24 13:19:56 +0000

    bug fix

--- myjournal/settings.py
+++ myjournal/settings.py
@@ -39,11 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'corsheaders',
 ]
 
 MIDDLEWARE = [
-    '**corsheaders.middleware.CorsMiddleware**',
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
