779f86e70ce1e70199835bf4709b87a677d8f40f
Daniel Molloy <danielmolloy_6@icloud.com>
2024-03-25 10:39:49 +0000

    loosened dotenv requirements

--- myjournal/settings.py
+++ myjournal/settings.py
@@ -82,16 +82,7 @@ WSGI_APPLICATION = 'myjournal.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-""" DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DATABASE_NAME'),
-        'USER': os.environ.get('DATABASE_USER'),
-        'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
-        'HOST': os.environ.get('DATABASE_HOST'),
-        'PORT': os.environ.get('DATABASE_PORT'),
-    }
-} """
+
 """ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
